905f689625ee71327380d394023053ffe64bfc11
Samuel193-h <sd4968331@gmail.com>
2026-05-04 19:05:39 +0000

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
+const db = require("./config/db");
+const fuelRoutes = require("./routes/fuel");
+const salesRoutes = require("./routes/sales"); // ✅ added
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/fuel", fuelRoutes);
+app.use("/sales", salesRoutes); // ✅ added
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Test route works");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
